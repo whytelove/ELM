3a5c303b349ca4c1a616a6dd0587b6c3c5a6cea9
piper <p4perrau@enib.fr>
2017-12-05 15:18:56 +0100

    lol

--- app/src/main/java/com/example/piper/elm/Main.java
+++ app/src/main/java/com/example/piper/elm/Main.java
@@ -3,21 +3,20 @@ package com.example.piper.elm;
         import android.content.res.Resources;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.widget.ImageView;
         import android.widget.TextView;
+
+        import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class Main extends AppCompatActivity {
 
+    ImageView imageView;
+    PhotoViewAttacher mAttacher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        Resources patate = getResources();
-        String text = patate.getString(R.string.app_name);
-        text = text.replace("bleu", "rouge");
-        TextView blabla = new TextView(this);
-        blabla.setText(patate.getText(R.string.HW));
-        setContentView(blabla);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        mAttacher = new PhotoViewAttacher(imageView);
     }
 }
