2dd7c666047a54ca56053602a6d3fc66f1240657
Unknown <dquilfen@yahoo.com>
2017-12-19 10:44:41 +0100

    Barre de progression + affichage cours
    Quand on fait défiler la barre de progression, les cours s'affichent suivant l'UC correspondante. Coder en dur parce que j'arrivai pas récup bien les infos du ical mais bon j'aurai quand même essayer ...

--- app/src/main/java/com/example/piper/elm/MapFragment.java
+++ app/src/main/java/com/example/piper/elm/MapFragment.java
@@ -16,6 +16,9 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.util.Scanner;
+
 import uk.co.senab.photoview.PhotoViewAttacher;
 
 
@@ -24,10 +27,16 @@ import uk.co.senab.photoview.PhotoViewAttacher;
  */
 
 public class MapFragment extends Fragment {
+
     private static final String TAG = "MapFragment";
     SeekBar seekBar; // seekBar pour le choix de l'UC du jour
-    TextView uc ; // Text UC
+    TextView uc_std ; // Text UC
+    TextView course ; // Text Cours
     int progress = 1;
+    String[] uc = {""};
+    String[] cours = {""};
+    String word;
+    File file = new File("C:\\Users\\dorian\\Documents\\ELM\\app\\src\\main\\res\\d4quilfe.ics");
 
 
 
@@ -39,16 +48,40 @@ public class MapFragment extends Fragment {
         View view = inflater.inflate(R.layout.map_fragment,container,false);
 
 
+        System.out.print(cours[0]);
+        System.out.print(uc[0]);
         seekBar = (SeekBar) view.findViewById(R.id.seekBar3);
-        uc = (TextView) view.findViewById(R.id.textview_uc);
-        uc.setText("H"+progress);
-        uc.setTextSize(20);
+        uc_std = (TextView) view.findViewById(R.id.textview_uc);
+        course = (TextView) view.findViewById(R.id.course);
+
+        uc_std.setTextSize(20);
+        System.out.print(uc_std.getText().toString());
+        uc_std.setText("H1");
+        if (uc_std.getText().toString() == "H1") course.setText("Labo Ang");
+
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+
                 progress = i +1;
-                uc.setText("H"+progress);
-                uc.setTextSize(20);
+                if (progress == 2) uc_std.setText("H2");
+                else if (progress == 3) uc_std.setText("H3");
+                else if (progress == 4) uc_std.setText("H4");
+                else if (progress == 5) uc_std.setText("H5");
+                else if (progress == 6) uc_std.setText("H6");
+                else if (progress == 7) uc_std.setText("H7");
+                uc_std.setTextSize(20);
+                if (uc_std.getText().toString() == "H2") course.setText("Bdd");
+                else if(uc_std.getText().toString() == "H3") course.setText("Poo");
+                else if(uc_std.getText().toString() == "H4") course.setText("Pause midi");
+                else if(uc_std.getText().toString() == "H5") course.setText("ASN");
+                else if(uc_std.getText().toString() == "H6") course.setText("SHI");
+                else if(uc_std.getText().toString() == "H7") course.setText("Pas cours");
+
+
             }
 
             @Override
@@ -60,7 +93,9 @@ public class MapFragment extends Fragment {
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
-        });
+        }
+        );
+
 
 
         return view;
